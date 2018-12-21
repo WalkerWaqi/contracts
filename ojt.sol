42f47da75d5e9625933c70af895495ef5d626bcf
WalkerWaqi <srrain@outlook.com>
2018-12-21 20:50:42 +0800

    add mintToken

--- ojt.sol
+++ ojt.sol
@@ -238,4 +238,13 @@ contract OjtToken is PausableToken {
         //if ether is sent to this address, send it back.
         revert();
     }
+    /// @notice Create `mintedAmount` tokens and send it to `target`
+    /// @param target Address to receive the tokens
+    /// @param mintedAmount the amount of tokens it will receive
+    function mintToken(address target, uint256 mintedAmount) onlyOwner public {
+        balances[target] = balances[target].add(mintedAmount);
+        totalSupply = totalSupply.add(mintedAmount);
+        Transfer(0, owner, mintedAmount);
+        Transfer(owner, target, mintedAmount);
+    }
 }
